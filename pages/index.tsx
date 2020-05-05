d51ca013e44613d213c9cdf9eb4bf1c76ec384ed
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-05-06 00:23:40 +0530

    chore: use next.js with typescript

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Abhijith Vijayan</title>
+      </Head>
+
+      <main>
+        <h3>Abhijith Vijayan</h3>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  )
+}
