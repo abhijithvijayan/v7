1647f3f802bc9246e88fed79b419756e45dc8500
Abhijith Vijayan [KUBRIC] <78354201+abhijith-vijayan@users.noreply.github.com>
2021-05-09 01:52:47 +0530

    feat: purge-css with tailwindcss
    Signed-off-by: Abhijith Vijayan [KUBRIC] <78354201+abhijith-vijayan@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  purge: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '1': '0 0 10px rgba(0,0,0,.2)',
+      },
+    },
   },
   variants: {},
   plugins: [],
