814142f950d73e602e72759d16820bb864d6cf2f
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2022-11-23 23:11:03 +0530

    refactor: remove unwanted routes

--- config/index.ts
+++ config/index.ts
@@ -16,32 +16,19 @@ export enum SiteProps {
   Icon96 = '/icons/96x96.png',
   Favicon = '/icons/favicon.ico',
 
-  Resume = '/resume.pdf',
   Profile = '/images/unicorn.png',
   Cover = '/images/activity.png',
 }
 
-export enum SocialLinks {
-  GitHub = 'https://github.com/abhijithvijayan/',
-  Twitter = 'https://twitter.com/_abhijithv',
-  LinkedIn = 'https://www.linkedin.com/in/iamabhijithvijayan/',
-  Medium = 'https://medium.com/@abhijithvijayan',
-  GitHubSponsors = 'https://github.com/sponsors/abhijithvijayan',
-  OpenCollective = 'https://opencollective.com/abhijithvijayan',
-  Patreon = 'https://www.patreon.com/abhijithvijayan',
-  PayPal = 'https://www.paypal.me/iamabhijithvijayan',
-  BuyMeACoffee = 'https://www.buymeacoffee.com/abhijithvijayan',
-}
-
-export enum Routes {
-  Root = '/',
-  Donate = '/donate',
-  Synapse = '/synapse',
-  GitHub = '/github',
-  Twitter = '/twitter',
-  LinkedIn = '/linkedin',
-  Patreon = '/patreon',
-  PayPal = '/paypal',
-  BuyMeACoffee = '/buymeacoffee',
-  OpenCollective = '/opencollective',
-}
+export const Routes ={
+  Root: '/',
+  Donate: '/donate',
+  Synapse: '/synapse',
+  GitHub: '/github',
+  Twitter: '/twitter',
+  LinkedIn: '/linkedin',
+  Patreon: '/patreon',
+  PayPal: '/paypal',
+  BuyMeACoffee: '/buymeacoffee',
+  OpenCollective: '/opencollective',
+} as const
